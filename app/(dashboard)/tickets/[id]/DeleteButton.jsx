9eff6729889c3d1bdc71a6149a2bf6931aa2c504
CodyJReed <cody2j2reed@gmail.com>
2023-08-13 22:54:46 -0700

    finish project with useTransition to include server action for onClick

--- app/(dashboard)/tickets/[id]/DeleteButton.jsx
+++ app/(dashboard)/tickets/[id]/DeleteButton.jsx
@@ -1,32 +1,14 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { deleteTicket } from "@/app/actions";
+import { useTransition } from "react";
 
 export default function DeleteButton({id}) {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
+  const [isPending, startTransition] = useTransition()
 
-  const handleClick = async () => {
-    setIsLoading(true);
-
-    const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
-      method: "DELETE",
-    });
-
-    const json = await res.json();
-
-    if (json.error) {
-      console.log(json.error);
-      setIsLoading(false);
-    } else if (!json.error) {
-      router.refresh();
-      router.push("/tickets");
-    }
-  };
   return (
-    <button className="btn-primary" onClick={handleClick} disabled={isLoading}>
-      {isLoading ? "Deleteing..." : "Delete Ticket"}
+    <button className="btn-primary" onClick={() => startTransition(() => deleteTicket(id))} disabled={isPending}>
+      {isPending ? "Deleteing..." : "Delete Ticket"}
     </button>
   );
 }
